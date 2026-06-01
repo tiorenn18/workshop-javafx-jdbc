509e425d153831cdcc8dab686de240988fb5bbf9
Tiorenn <ericorenanleal124@gmail.com>
2026-06-01 17:27:52 -0300

    DepartmentForm(dialog) design

---
+++ workshop-javafx-jdbc/src/main/java/workshop/javafx/jdbc/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package workshop.javafx.jdbc.gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+    
+    public static Stage currentStage(ActionEvent event) {
+        return (Stage) ((Node)event.getSource()).getScene().getWindow();
+    }
+}
