64f7d80eb7562c684ad029b13f2af06b30cbeafd
Tiorenn <ericorenanleal124@gmail.com>
2026-06-03 16:39:06 -0300

    Saving a new Department

--- workshop-javafx-jdbc/src/main/java/workshop/javafx/jdbc/DepartmentFormController.java
+++ workshop-javafx-jdbc/src/main/java/workshop/javafx/jdbc/DepartmentFormController.java
@@ -3,17 +3,25 @@ package workshop.javafx.jdbc;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import workshop.javafx.jdbc.db.DbException;
+import workshop.javafx.jdbc.gui.util.Alerts;
 import workshop.javafx.jdbc.gui.util.Constraints;
+import workshop.javafx.jdbc.gui.util.Utils;
 import workshop.javafx.jdbc.model.entities.Department;
+import workshop.javafx.jdbc.model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -35,14 +43,46 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
-    @FXML
-    public void onBtSaveAction() {
-        System.out.println("onBtSaveAction");
+    @SuppressWarnings("exports")
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @FXML
-    public void onBtCancelAction() {
-        System.out.println("onBtCancelAction");
+    @SuppressWarnings("exports")
+    public void onBtSaveAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+
+        } catch (DbException e) {
+            Alerts.showAlerts("Error saving object", null, e.getMessage(), AlertType.ERROR);
+        }
+
+    }
+
+    @SuppressWarnings("exports")
+    public Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
+    }
+
+    @FXML
+    @SuppressWarnings("exports")
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
     @Override
@@ -50,7 +90,7 @@ public class DepartmentFormController implements Initializable {
         initializeNodes();
     }
 
-    private void initializeNodes() { 
+    private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
     }
@@ -63,4 +103,5 @@ public class DepartmentFormController implements Initializable {
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
     }
+
 }
--- workshop-javafx-jdbc/src/main/java/workshop/javafx/jdbc/model/service/DepartmentService.java
+++ workshop-javafx-jdbc/src/main/java/workshop/javafx/jdbc/model/service/DepartmentService.java
@@ -7,10 +7,19 @@ import workshop.javafx.jdbc.model.dao.DepartmentDao;
 import workshop.javafx.jdbc.model.entities.Department;
 
 public class DepartmentService {
-    
+
     private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
     public List<Department> findAll() {
         return dao.findAll();
+    }
+
+    public void saveOrUpdate(Department obj) {
+        if (obj.getId() == null) {
+            dao.insert(obj);
+            
+        } else {
+            dao.update(obj);
+        }
     }
 }
